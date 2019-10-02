eb89fa5382cbab3eef02445ef03973784cd77a5d
LucasRR94 <lucasregodarocha@outlook.com>
2019-10-02 13:32:12 -0300

    finish tests of control_game

--- tests/test_Control_game.py
+++ tests/test_Control_game.py
@@ -56,8 +56,6 @@ def test_check_fisher_live(standartinput):
 	assert check_fisher_live(fisher2) == 0
 	assert check_fisher_live(first) == None
 
-
-
 @pytest.mark.parametrize("standartinput",[("fish1",90,Weapon("weapon1",10,10),Defense("defense1",10,10),"0.0.0.0"),("fish2",100,Weapon("weapon2",1,1),None,"1.1.1.1")])
 def test_insertIntoMap(standartinput):
 	first = create_context_map()
@@ -118,7 +116,75 @@ def test_insertIntoMap(standartinput):
 
 @pytest.mark.parametrize("standartinput",[("fish1",90,Weapon("weapon1",10,10),Defense("defense1",10,10),"0.0.0.0"),("fish2",100,Weapon("weapon2",1,1),None,"1.1.1.1")])
 def test_perform_instruction(standartinput):
-	pass
+	first = create_context_map()
+	mainmap,totalspells = first[0]
+	dict_fishers = first[1]
+	mainmap,fisher1 = insertIntoMap(standartinput[0],standartinput[1],standartinput[2],standartinput[3],mainmap,standartinput[4],dict_fishers)
+	assert type(perform_instruction(None,fisher1,0,None)) is str 
+	assert perform_instruction(None,fisher1,0,None) == "No Items on backpack"
+	assert type(perform_instruction(None,fisher1,3,None)) is str 
+	assert perform_instruction(None,fisher1,3,None).index("Item") >= 0
+	vec = perform_instruction(None,fisher1,3,None).split(":")
+	vec2 = vec[1]
+	vec = vec2.split("\n")
+	assert perform_instruction(vec[0],fisher1,2,None) == 1
+	assert (perform_instruction(None,fisher1,0,None)).index(vec[0])>=0
+	assert type(perform_instruction(vec[0],fisher1,1,None)) is int
+	assert type(perform_instruction(None,fisher1,1,None)) is int
+	a = perform_instruction(None,fisher1,1,None)
+	if(a !="No Items on backpack"):
+		assert type(perform_instruction(None,fisher1,4,None)) is list
+	
+	assert type(perform_instruction(None,fisher1,5,None)) is str
+	assert (perform_instruction(None,fisher1,5,None)).index("island") > 0
+	count = 0 
+	a = perform_instruction(None,fisher1,5,None)
+	if(a == "Spells available in island"):
+		count+=1
+	if(a == "No spell present on the island"):
+		count+=1
+	assert count == 1, "Error, verify spells"
+	assert type(perform_instruction(None,fisher1,7,None)) is str		
+	count = 0
+	for i in range(4):
+		a = perform_instruction(None,fisher1,7,None)
+		if(a.index("island")>=0):
+			try:
+				a.index("right")>=0
+			except ValueError:
+				try:
+					a.index("center")>=0
+				except ValueError:
+					try:
+						a.index("back")>=0
+					except ValueError:
+						try:
+							a.index("left")
+						except ValueError:
+							raise Exception("None directions found!!!")
+						else:
+							count+=1
+					else:
+						count+=1
+				else:
+					count+=1
+
+			else:
+				count+=1
+			
+	assert count >=1, "Error, providing directions on islands"
+	l = a.split(" ")
+	assert (perform_instruction(l[1],fisher1,6,None)) == 1
+	assert (perform_instruction(None,fisher1,8,None)) == "Sucess on collect spell\n"
+	assert (perform_instruction(None,fisher1,10,None)).index("Name of individual")>=0
+	string_enimies = (perform_instruction(None,fisher1,10,None))
+	final_name_enemies = string_enimies.split(":")
+	name_enemie = final_name_enemies[1]
+	final_name_enemies = name_enemie.split("\n")[0]
+	assert perform_instruction(final_name_enemies,fisher1,9,dict_fishers) == 1
+	assert (perform_instruction(None,fisher1,11,None)).index(fisher1.getName())>=0
+	assert (perform_instruction(fisher1.getName(),fisher1 ,12,dict_fishers)) == 1
+	assert len(dict_fishers) == 0
 
 @pytest.mark.parametrize("standartinput",[("fish1",90,Weapon("weapon1",10,10),Defense("defense1",10,10),"0.0.0.0",Spell("Spell 1",10),Spell("Spell 2",11),Defense("Defesa s1",10,2),Weapon("Weapon",3,5)),("fish2",100,Weapon("weapon2",1,1),None,"1.1.1.1",Spell("Spell 1",10),Spell("Spell 2",11),Defense("Defesa s1",1,5),Weapon("Weapona",3,7))])
 def test_disconnect_player(standartinput):
@@ -185,3 +251,4 @@ def test_eliminate_fisher_deads(standartinput):
 		pass
 	else:
 		raise Exception("Error, should be removed!!!")
+
